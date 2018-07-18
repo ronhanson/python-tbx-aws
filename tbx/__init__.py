101d32bd8cadff644c1205edea9eaefbb88aa593
Ronan Delacroix <ronan.delacroix@gmail.com>
2018-07-18 15:02:23 +0200

    Fix (v0.1.2)

--- tbx/__init__.py
+++ tbx/__init__.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu
 """
-(c) 2014 Ronan Delacroix
-TBX Lazy Sub-module Loading
+(c) 2018 Ronan Delacroix
 :author: Ronan Delacroix
 """
-from . import code
-code.lazy_load_module(__name__)
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
